cf6d22d8f421c6c75eef998655a8a1650e3281fb
saurabh Jambhorkar <130629194+Saurabhsj123@users.noreply.github.com>
2024-06-13 20:04:05 +0530

    changes

--- Config/db.js
+++ Config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+const env = require('dotenv/config')
 
-mongoose.connect("mongodb://127.0.0.1:27017/Food-Delivery")
+mongoose.connect(process.env.DATABASE_URL)
 
 const connectDB = mongoose.connection;
 
