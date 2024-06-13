7b811ebc6263166232a2f281ed9f28a766c44f65
saurabh Jambhorkar <130629194+Saurabhsj123@users.noreply.github.com>
2024-06-13 17:17:49 +0530

    changes

--- Config/db.js
+++ Config/db.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://127.0.0.1:27017/Food-Delivery", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect("mongodb://127.0.0.1:27017/Food-Delivery")
 
 const connectDB = mongoose.connection;
 
